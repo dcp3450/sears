060404463e770cf9e530bb9ed5b27bdc8531b1b5
David Powell <dcp3450@gmail.com>
2015-01-24 03:56:05 -0500

    added view

--- searshackathon/platforms/android/assets/www/js/desktop.js
+++ searshackathon/platforms/android/assets/www/js/desktop.js
@@ -1,11 +1,19 @@
 function menuAction(e){
-    e.parentNode.parentNode.className = "lowerScreen";
-    e.parentNode.parentNode.innerHTML = e.parentNode.parentNode.innerHTML+'<div id="cover" onclick="menuClose(this)"></div>';
+    var nodeContainer = e.parentNode.parentNode;
+    nodeContainer.className = "lowerScreen";
+    nodeContainer.setAttribute("style","overflow:hidden");
+    nodeContainer.innerHTML = nodeContainer.innerHTML+'<div id="cover" onclick="menuClose(this,testMe)"></div>';
 }
 
-function menuClose(e) {
-    e.parentNode.className = "";
+function testMe(e){
+    setTimeout(function(){ e.setAttribute("style",""); }, 600);
+}
+
+function menuClose(e,callback) {
+    var nodeContainer = e.parentNode;
+    nodeContainer.className = "";
     e.remove();
+    callback(nodeContainer);
 }
 
 function triggerView(view){
