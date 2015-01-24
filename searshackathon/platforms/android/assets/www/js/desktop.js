e5199fc2799ab708c1752cb637496606ffcdb1a4
David Powell <dcp3450@gmail.com>
2015-01-24 15:06:30 -0500

    changes

--- searshackathon/platforms/android/assets/www/js/desktop.js
+++ searshackathon/platforms/android/assets/www/js/desktop.js
@@ -13,6 +13,8 @@ function menuClose(e,callback) {
     var nodeContainer = e.parentNode;
     nodeContainer.className = "";
     e.remove();
+
+    callback(nodeContainer);
 }
 
 function triggerView(view){
@@ -85,8 +87,6 @@ var addRippleEffect = function (e) {
 }
 
 document.addEventListener('click', addRippleEffect, false);
-    callback(nodeContainer);
-}
 
 function triggerView(view){
 
